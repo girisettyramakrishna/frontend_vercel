ddd4b629d5d538c23f7f0b6050c0a57bba28da08
girisettyramakrishna <girisettyramakrishna4@gmail.com>
2025-09-01 16:43:58 +0530

    Update API's.js

--- react_redux/src/API's.js
+++ react_redux/src/API's.js
@@ -1,7 +1,8 @@
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "https://backend-render-1-279z.onrender.com";
+    : "https://backend-render-3-pdxs.onrender.com";
 
 export const registerAPI = `${BASE_URL}/api/auth/register`;
 export const loginAPI = `${BASE_URL}/api/auth/login`;
+
